1f58cdfaacaa0d937ae4ae184750ea1f72355423
alexanderbogdanov <ironweed@list.ru>
2022-04-29 01:07:48 +0300

    Point

---
+++ sandbox/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(10.0, 4.0);
+        Point p2 = new Point(7.0, 2.0);
+        System.out.println(p1.distance(p2));
+    }
+
+
+}
