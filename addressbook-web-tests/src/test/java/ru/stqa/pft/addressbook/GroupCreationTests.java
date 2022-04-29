0aa49c64343b0969265fc94e7e467cb06822a3ae
alexanderbogdanov <ironweed@list.ru>
2022-04-29 20:02:01 +0300

    some minor changes

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,14 +1,16 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.fail;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GroupCreationTests {
   private WebDriver wd;
@@ -97,6 +99,7 @@ public class GroupCreationTests {
       return false;
     }
   }
+
 
 
 }
