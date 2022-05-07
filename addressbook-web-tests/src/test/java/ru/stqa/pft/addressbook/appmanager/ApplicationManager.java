f73b1903373b316f1002c1a61463ab1f0f46a058
alexanderbogdanov <ironweed@list.ru>
2022-05-08 01:27:14 +0300

    multiple browsers implementation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.appmanager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,10 +16,27 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    WebDriverManager.chromedriver().setup();
-    wd = new ChromeDriver();
+    switch (browser) {
+      case BrowserType.CHROME:
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+        break;
+      case BrowserType.FIREFOX:
+        WebDriverManager.firefoxdriver().setup();
+        wd = new FirefoxDriver();
+        break;
+      case BrowserType.OPERA_BLINK:
+        WebDriverManager.operadriver().setup();
+        wd = new OperaDriver();
+        break;
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
     contactHelper = new ContactHelper(wd);
