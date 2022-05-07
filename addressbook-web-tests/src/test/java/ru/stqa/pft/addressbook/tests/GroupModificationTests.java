786fc730b01a3520d92ef25fe2d21ed6c7529abe
alexanderbogdanov <ironweed@list.ru>
2022-05-07 13:52:32 +0300

    group modification test implemented

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupsPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("33", "44", "55"));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().returnToGroupsPage();
+
+
+
+
+  }
+
+
+}
