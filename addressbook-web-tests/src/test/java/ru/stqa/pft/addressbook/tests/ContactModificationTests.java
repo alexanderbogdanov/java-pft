427bd609a6a3886834f6fc72b677c0eafd4e51ae
alexanderbogdanov <ironweed@list.ru>
2022-05-09 17:47:02 +0300

    some some_preconditions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,11 +2,24 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
+    app.getNavigationHelper().gotoHomePage();
+    if (!app.getContactHelper().isThereAContact()) {
+      app.getNavigationHelper().gotoGroupsPage();
+      if (!app.getGroupHelper().isThereAGroup()) {
+        app.getNavigationHelper().gotoGroupsPage();
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+      }
+      app.getNavigationHelper().gotoHomePage();
+      app.getContactHelper().createContact(new ContactData("Alex", "Shmalex",
+              "Home sweet home", "3223322",
+              "alex@shmalex.com", "test1"));
+    }
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("Johnny", "Doe",
