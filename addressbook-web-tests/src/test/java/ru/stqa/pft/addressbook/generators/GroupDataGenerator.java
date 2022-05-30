628f30cf744e097efa9dd997e1d87f919fbb7d6c
alexanderbogdanov <ironweed@list.ru>
2022-05-30 13:36:02 +0300

    xml text data generation for group creation test implemented

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.generators;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -19,6 +20,9 @@ public class GroupDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
     JCommander.newBuilder()
@@ -30,14 +34,28 @@ public class GroupDataGenerator {
 
   private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv")) {
+      saveAsCSV(groups, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXML(groups, new File(file));
+    } else {
+      System.out.println("Format unrecognized " + format);
+    }
+  }
 
+  private void saveAsXML(List<GroupData> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
   }
 
 //  int count = Integer.parseInt(args[0]);
 //  File file = new File(args[1]);
 
-  private void save(List<GroupData> groups, File file) throws IOException {
+  private void saveAsCSV(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
       writer.write(String.format("%s;%s;%s\n",
