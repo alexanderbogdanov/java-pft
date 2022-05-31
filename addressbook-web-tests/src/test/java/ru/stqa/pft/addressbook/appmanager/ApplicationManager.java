648e74175e94ca3e5ec66eeef9f86188befd9780
alexanderbogdanov <ironweed@list.ru>
2022-05-31 19:39:01 +0300

    откатился кхерам изза того что баранцеву лень обновить курс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,46 +4,53 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.BrowserType;
 
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
-
-import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+  private final Properties properties;
   WebDriver wd;
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private final String browser;
+  private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
+    properties = new Properties();
+
   }
 
-  public void init() {
-    if ("CHROME".equals(browser)) {
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+    if (browser.equals(BrowserType.CHROME)) {
       WebDriverManager.chromedriver().setup();
       wd = new ChromeDriver();
-    } else if ("FIREFOX".equals(browser)) {
+    } else if (browser.equals(BrowserType.FIREFOX)) {
       WebDriverManager.firefoxdriver().setup();
       wd = new FirefoxDriver();
-    } else if ("OPERA".equals(browser)) {
-      WebDriverManager.chromedriver().setup();
+    } else if (browser.equals(BrowserType.EDGE)) {
+      WebDriverManager.edgedriver().setup();
     }
 
-    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-    wd.get("http://localhost/addressbook/index.php");
+//    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
     contactHelper = new ContactHelper(wd);
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
   public void stop() {
