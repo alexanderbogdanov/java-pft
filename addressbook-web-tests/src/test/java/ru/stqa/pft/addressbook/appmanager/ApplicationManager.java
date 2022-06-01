d467b67c7cd3bd3f183a1ce9be69b4ce4dce2fcc
alexanderbogdanov <ironweed@list.ru>
2022-06-01 16:29:40 +0300

    dbHelper for getting groups and contacts from db created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private DbHelper dbHelper;
   private String browser;
 
   public ApplicationManager(String browser) throws IOException {
@@ -33,6 +34,9 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
+
+    dbHelper = new DbHelper();
+
     if (browser.equals(BrowserType.CHROME)) {
       WebDriverManager.chromedriver().setup();
       wd = new ChromeDriver();
@@ -68,5 +72,9 @@ public class ApplicationManager {
 
   public ContactHelper contact() {
     return contactHelper;
+  }
+
+  public DbHelper db() {
+    return dbHelper;
   }
 }
